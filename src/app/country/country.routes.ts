4a35fb626d79362be0cfe18dc7f10f18052978e7
BigCode07 <bigcode07@gmail.com>
2025-03-15 12:57:56 -0300

    Rutas padres y template html

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
-export const countryRoute: Routes;
+export const countryRoute: Routes = [
+  {
+    path: '',
+    component: CountryPageComponent,
+  },
+];
