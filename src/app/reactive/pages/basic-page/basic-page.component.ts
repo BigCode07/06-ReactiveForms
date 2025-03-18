36acaa87837e29615e6893215f6b4a1a5166d24d
BigCode07 <bigcode07@gmail.com>
2025-03-17 23:54:28 -0300

    Se utilizo form utils para agregar logica y liberar codigo en el componente ts

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,8 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormUtils } from '../../../utils/form-utils';
 import {
   FormBuilder,
-  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
@@ -10,45 +10,62 @@ import {
 
 @Component({
   selector: 'app-basic-page',
+  standalone: true, // Agregado para evitar errores en Angular Standalone
   imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent {
-  //*Esta es la forma utilizando formBuilder
   private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
   myForm: FormGroup = this.fb.group({
-    name: ['', Validators.required, Validators.minLength(3)],
+    name: ['', [Validators.required, Validators.minLength(3)]], // Corregido
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
-  //*Esta es la forma utilizando formGroup
-  // myForm = new FormGroup({
-  //   name: new FormControl(''),
-  //   price: new FormControl(0),
-  //   inStorage: new FormControl(0),
-  // });
+  // isValidField(fieldName: string): boolean {
+  //   const field = this.myForm.controls[fieldName];
+  //   return !!(field.errors && field.touched);
+  // }
 
-  isValidField(fieldName: string): boolean | null {
-    return !!this.myForm.controls[fieldName].errors;
-  }
+  // getFieldError(fieldName: string): string | null {
+  //   const control = this.myForm.controls[fieldName];
 
-  getFieldError(fieldName: string): string | null {
-    if (this.myForm.controls[fieldName]) return null;
-    const errors = this.myForm.controls['fieldName'].errors ?? {};
-    for (const key of Object.keys(errors)) {
-      switch (key) {
-        case 'required':
-          return 'Este campo es requerido';
+  //   if (!control || !control.errors) return null;
 
-        case 'minlength':
-          return `Minimo de ${errors['minLength'].requiredLength} caracteres`;
+  //   const errors = control.errors;
 
-        case 'min':
-          return `Valor minimo de ${errors['min'].min}`;
-      }
+  //   for (const key of Object.keys(errors)) {
+  //     switch (key) {
+  //       case 'required':
+  //         return 'Este campo es requerido';
+
+  //       case 'minlength':
+  //         return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
+
+  //       case 'min':
+  //         return `Valor mínimo de ${errors['min'].min}`;
+
+  //       case 'max':
+  //         return `Valor máximo de ${errors['max'].max}`;
+  //     }
+  //   }
+
+  //   return null;
+  // }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
     }
 
-    return null;
+    console.log('Formulario enviado:', this.myForm.value);
+
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
   }
 }
